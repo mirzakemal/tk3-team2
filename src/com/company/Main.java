c714604a34b22e2012e7cfa54f1d3486b3a3f45b
mita <mitadaniswari@gmail.com>
2022-04-23 22:53:43 +0700

    implement_requirements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,116 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void puzzleGame(){
         Scanner input = new Scanner(System.in);
+        int final_score;
+
+        /** Variable Level 1 **/
+        int level =1;
+        ArrayList<String> listOfAnswer_1 = new ArrayList<>();
+        listOfAnswer_1.add("die");
+        listOfAnswer_1.add("led");
+        listOfAnswer_1.add("lei");
+        listOfAnswer_1.add("let");
+        listOfAnswer_1.add("lid");
+        listOfAnswer_1.add("lie");
+        listOfAnswer_1.add("lit");
+        listOfAnswer_1.add("tie");
+        listOfAnswer_1.add("deli");
+        listOfAnswer_1.add("diet");
+        listOfAnswer_1.add("edit");
+        listOfAnswer_1.add("idle");
+        listOfAnswer_1.add("lied");
+        listOfAnswer_1.add("tide");
+        listOfAnswer_1.add("tied");
+        listOfAnswer_1.add("tile");
+        listOfAnswer_1.add("tilt");
+        listOfAnswer_1.add("tilde");
+        listOfAnswer_1.add("tiled");
+        listOfAnswer_1.add("title");
+        listOfAnswer_1.add("titled");
+        listOfAnswer_1.add("titled");
+
+        /** Variable level 2 **/
+
+        /** variable Level 3 **/
+
+        System.out.println("Coepoe Word Puzzle");
+        System.out.println("===================");
+        System.out.println("Rules :");
+        System.out.println("1. Create a word using given characters, min 3 characters & max 6 characters.");
+        System.out.println("2. Each Level, you have 10 chance to answer correctly.");
+        System.out.println("3. To get through to next level, you have to score 70 points each level.");
+
+        // level 1
+        final_score = game(level, listOfAnswer_1);
+        if(final_score >= 70){ // 70 passed
+
+            //masuk level 2
+            level = level+1;
+            int skor_lv_2 = game(level, listOfAnswer_1);
+            final_score=final_score+skor_lv_2;
+            if(final_score >= 140){ // minamal level 1+ minimal level 2 = passed
+
+                //level 3
+                level = level+1;
+                int score_lv_3 = game(level, listOfAnswer_1);
+                final_score = final_score +score_lv_3;
+                    if(final_score >= 210){ // minimal level 3 + minimal level 2 + minimal level 1 = 210, sehinggal level 3 = 70 aja udah passed
+                        System.out.println("Overall Score = "+final_score);
+                        System.out.println("You Win !!");
+                    }else{
+                         System.out.println("Do you want to retry [y/t]?");
+                         String ulang = input.nextLine();
+                         if (ulang.equalsIgnoreCase("y")){
+                             puzzleGame();
+                         }else{
+                             System.out.println("Game End");
+                         }
+                    }
+            }else{
+                //balik ke level 1
+                level=level-1;
+                game(level, listOfAnswer_1);
+            }
+        }else{
+            //balik ke level 1
+            game(level, listOfAnswer_1);
+        }
+    }
+    public static int game(int level, ArrayList<String> listOfAnswer){
+        int countScore = 0;
+
+        System.out.println("=== level "+level+" ===");
+        System.out.println("  d   e   t   t   l   i  ");
+        Scanner input = new Scanner(System.in);
         try{
-            ArrayList<String []> listOfAnswer = new ArrayList<>();
-            // Initialize child array
-            String [] answerOne = {""};
-            String [] answerTwo = {""};
-            String [] answerThree = {""};
-            listOfAnswer.add(answerOne);
-            listOfAnswer.add(answerTwo);
-            listOfAnswer.add(answerThree);
-            int correctAnswer1 = 0;
-            int correctAnswer2 = 0;
-            int correctAnswer3 = 0;
-            for(int i = 0; i < listOfAnswer.size(); i++ ){
                 ArrayList<String> poolAnswer = new ArrayList<>();
-                int countScore = 0;
-                for(int j=0; j < 10; j++){
+
+                int correctAnswer = 0;
+
+            for(int j=0; j < 10; j++){
                     String inputJawaban = input.nextLine();
-                    boolean contains1 = Arrays.stream(answerOne).anyMatch(x -> x == inputJawaban);
-                    boolean contains2 = Arrays.stream(answerTwo).anyMatch(x -> x == inputJawaban);
-                    boolean contains3 = Arrays.stream(answerThree).anyMatch(x -> x == inputJawaban);
                     int number = j+1;
                     System.out.println(number +"> Your answer : " + inputJawaban);
-                    if(i == 0 && contains1){
-                        correctAnswer1 += 10;
-                        System.out.println("#Right. Score : " + correctAnswer1);
-                        poolAnswer.add(inputJawaban);
-                        countScore++;
-                    } else if(i == 1 && contains2){
-                        correctAnswer2 += 10;
-                        System.out.println("#Right. Score : " + correctAnswer2);
-                        poolAnswer.add(inputJawaban);
-                        countScore++;
-                    } else if(i == 2 && contains3){
-                        correctAnswer3 += 10;
-                        System.out.println("#Right. Score : " + correctAnswer3);
-                        countScore++;
-                        poolAnswer.add(inputJawaban);
-                    } else if(poolAnswer.contains(inputJawaban)){
+                    boolean contains = listOfAnswer.contains(inputJawaban);
+                    if(poolAnswer.contains(inputJawaban)){
                         System.out.println("You had already type this word before...");
-                        j--;
+                    }else if(contains){
+                        countScore += 10;
+                        System.out.println("#Right. Score : " + countScore);
+                        poolAnswer.add(inputJawaban);
+                        correctAnswer++;
                     }
                 }
-                if(countScore < 7) throw new ArithmeticException("You lose try!! Try again...");
-                System.out.println("You had answered 10 times with " + countScore + " right answers...");
+            if(correctAnswer < 7) {
+                throw new ArithmeticException("You lose try!! Try again... \nYou had answered 10 times with "+ correctAnswer +" right answers..");
+            }else{
+                System.out.println("You had answered 10 times with " + correctAnswer + " right answers...");
+                return countScore;
             }
         }catch (ArithmeticException e){
             System.out.println(e.getMessage());
@@ -58,8 +118,10 @@ public class Main {
             String ulang = input.nextLine();
             if (ulang.equalsIgnoreCase("y")) puzzleGame();
         }
+        return countScore;
     }
     public static void main(String[] args) {
+
         puzzleGame();
     }
 }
